0e2378de16c876c3583cc8f9a43517fadb7329a6
Ilya Medvedev <medved55rus@gmail.com>
2016-03-17 21:29:17 +0600

    move vertex cache to tmp dir

--- vertx/src/main/java/medvedev/ilya/example/async/vertx/Main.java
+++ vertx/src/main/java/medvedev/ilya/example/async/vertx/Main.java
@@ -2,17 +2,31 @@ package medvedev.ilya.example.async.vertx;
 
 import io.vertx.core.Verticle;
 import io.vertx.core.Vertx;
+import io.vertx.core.impl.FileResolver;
 import medvedev.ilya.example.async.ping.Repository;
 import medvedev.ilya.example.async.ping.PongRepository;
 import medvedev.ilya.example.async.vertx.verticle.MainVerticle;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 public class Main {
+    private static final String VERTX_CACHE_DIR = "vertx-cache";
     private static final int THREAD_POOL_SIZE = 2;
 
     public static void main(String[] args) {
+        final String cacheDir;
+        try {
+            cacheDir = Files.createTempDirectory(VERTX_CACHE_DIR)
+                    .toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.setProperty(FileResolver.CACHE_DIR_BASE_PROP_NAME, cacheDir);
+
         final Executor executor = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
         final Repository repository = new PongRepository(executor);
         final Verticle mainVerticle = new MainVerticle(repository);
--- vertx/src/main/java/medvedev/ilya/example/async/vertx/verticle/MainVerticle.java
+++ vertx/src/main/java/medvedev/ilya/example/async/vertx/verticle/MainVerticle.java
@@ -10,7 +10,12 @@ import medvedev.ilya.example.async.ping.Repository;
 import medvedev.ilya.example.async.vertx.handler.EchoHandler;
 import medvedev.ilya.example.async.vertx.handler.PingHandler;
 
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class MainVerticle extends AbstractVerticle {
+    private static final String VERTX_UPLOAD_DIR = "vertx-uploads";
+
     private static final String PING = "/ping";
     private static final String ECHO = "/echo";
 
@@ -30,7 +35,15 @@ public class MainVerticle extends AbstractVerticle {
         router.route(HttpMethod.GET, PING)
                 .handler(ping);
 
-        final Handler<RoutingContext> body = BodyHandler.create();
+        final String fileUploadDir;
+        try {
+            fileUploadDir = Files.createTempDirectory(VERTX_UPLOAD_DIR)
+                    .toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        final Handler<RoutingContext> body = BodyHandler.create(fileUploadDir);
         final Handler<RoutingContext> echo = new EchoHandler();
 
         router.route(HttpMethod.POST, ECHO)
